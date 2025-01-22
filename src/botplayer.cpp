ed19f7e43c68a912091d03d4aa808d4c398be697
dani-neto <daniellesantananeto2@gmail.com>
2025-01-22 07:35:24 -0300

    Create botplayer.cpp

---
+++ src/botplayer.cpp
@@ -0,0 +1,12 @@
+#include "botplayer.hpp"
+#include <iostream>
+
+// Implementação da IA para Jogo da Velha
+    // Lógica do MiniMax ou outra estratégia
+    
+// Implementação da IA para Lig 4
+    // Lógica do MiniMax com poda alfa-beta
+ 
+// Implementação da IA para Reversi
+    // Lógica baseada em heurística de posição
+  
